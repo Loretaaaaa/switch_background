2044a260ed0dd7e55e3a7afd7329a7ddce4a2997
Loreta Badalyan <131776572+Loretaaaaa@users.noreply.github.com>
2023-05-09 18:19:56 +0400

    Add files via upload

---
+++ switch_background/index.js
@@ -0,0 +1,12 @@
+// const colors = ["#ff6666", "#ffa366", "#ffff66", "#66ff66", "#66ffff", "#6666ff", "#ff66ff"];
+// let currentIndex = 0;
+
+// function changeColor() {
+//   document.body.style.backgroundColor = colors[currentIndex];
+//   currentIndex = (currentIndex + 1) % colors.length;
+// }
+
+// setInterval(changeColor, 1000);
+function changeColor(color) {
+    document.body.style.backgroundColor = color;
+    }
